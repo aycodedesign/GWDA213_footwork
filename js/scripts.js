d65d172f21f94fae04a3a8d45319511b13de16f6
el3ctricSol <aubrey.ky.young@gmail.com>
2018-02-13 16:42:33 -0800

    Week 5 complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,42 @@
-// console.log('You Got me!! Partner!!');
-
-/*-------------------------------------
-| Clouds Animation
--------------------------------------*/
-function cloud_animate() {
-    $('.clouds')
-        .css({'background-position-x':'0px'})
-        .animate({'background-position-x':'-1300px'}, 10000, 'linear', cloud_animate); //keeps it going
-}
-
-// function call - starts the party
-// cloud_animate();
-
 /*-------------------------------------
 | Set The Stage :: Slide 1
 -------------------------------------*/
 function set_the_stage() {
-    $('.bruce-lee.bruce-lee').stop().css({'right':'-300px'});
-    $('.michael-jackson.michael-jackson').stop().css({'right':'-260px'});
+    $('.start').stop().css({'top':'0px'});
 
-    $('.bruce-lee.heavy-bag').stop().css({'left':'-200px'});
-    $('.michael-jackson.mic-stand').stop().css({'bottom':'-210px','left': '400px'});
+    $('.bl.dojo').stop().css({'bottom':'-600px'});
+    $('.bl.bruce-lee').stop().css({'left':'-260px'});
+    $('.bl.heavy-bag').stop().css({ 'right': '-185px' });
+    $('.bl.yin-yang').stop().css({ 'top': '-210px' });
+
+    $('.mj.stage').stop().css({ 'top': '-700px' });
+    $('.mj.michael-jackson').stop().css({'right':'-260px'});
+    $('.mj.mic-stand').stop().css({ 'bottom': '-210px', 'left': '400px' });
+    $('.mj.disco-ball').stop().css({ 'top': '-270px' });
+
+    $('.ma.boxing-ring').stop().css({'bottom':'-600px'});
+    $('.ma.muhammad-ali').stop().css({ 'right': '-420px' });
+    $('.ma.speed-bag').stop().css({ 'top': '-210px' });
+    $('.ma.fight-bell').stop().css({ 'top': '-235px' });
 }
 
 set_the_stage();
 
-// /*-------------------------------------
-// | Slide 2 :: Bruce Lee
-// -------------------------------------*/
+/*-------------------------------------
+| Slide 2 :: Bruce Lee
+-------------------------------------*/
 function slide2() {
-    $('.bruce-lee.heavy-bag').animate({ 'left': '350px' }, 1500);
-    $('.bruce-lee.bruce-lee').animate({'right':'450px'}, 250);
-    
+    $('.start').stop().css({ 'top': '-800px' });
+
+    $('.bl.dojo').animate({ 'bottom': '0px' }, 500);
+    $('.bl.bruce-lee').animate({ 'left': '150px', 'top':'180px' }, 700);
+    $('.bl.heavy-bag').animate({ 'right': '200px' }, 1000);
+    $('.bl.yin-yang').animate({ 'top': '-5px' }, 2000);
+
+    $('.mj.stage').animate({ 'top': '-700px' }, 500);
+    $('.mj.michael-jackson').animate({ 'right': '-400px' }, 500);
+    $('.mj.mic-stand').animate({ 'bottom': '-210px' }, 500);
+    $('.mj.disco-ball').animate({ 'top': '-270px' }, 500);
 }
 
 // slide2();
@@ -40,18 +45,46 @@ function slide2() {
 | Slide 3 :: Michael Jackson
 -------------------------------------*/
 function slide3() {
-    $('.bruce-lee.heavy-bag').animate({'left': '-200px' }, 1000);
-    $('.bruce-lee.bruce-lee').animate({'left':'-265px'}, 1000);
-    
+    $('.start').stop().css({ 'top': '-800px' });
 
-    $('.michael-jackson.mic-stand').animate({'bottom': '100px', 'bottom':'0px', 'left': '400px'}, 1500);
-    $('.michael-jackson.michael-jackson').animate({'right':'400px'}, 3500);
-    
+    $('.bl.dojo').animate({ 'bottom': '-600px' }, 500);
+    $('.bl.bruce-lee').animate({ 'left': '-260px' }, 700);
+    $('.bl.heavy-bag').animate({'right': '775px' }, 700);
+    $('.bl.yin-yang').animate({ 'top': '-210px' }, 500);
+
+
+    $('.mj.stage').animate({ 'top': '-100px' }, 500);
+    $('.mj.mic-stand').animate({'bottom': '150px'}, 750);
+    $('.mj.mic-stand').animate({ 'bottom': '0px' }, 250);
+    $('.mj.michael-jackson').animate({'right':'400px'}, 2000);
+    $('.mj.disco-ball').animate({ 'top': '-15px' }, 2000);
+
+    $('.ma.boxing-ring').animate({ 'bottom': '-600px' }), 500;
+    $('.ma.muhammad-ali').animate({ 'right': '-415px' }, 500);
+    $('.ma.speed-bag').animate({ 'top': '-210px' }, 300);
+    $('.ma.fight-bell').animate({ 'top': '-270px' }, 500);
 }
+
+// slide3();
 
 /*-------------------------------------
 | Slide 4 :: Muhammad Ali
 -------------------------------------*/
+function slide4() {
+    $('.start').stop().css({ 'top': '-800px' });
+
+    $('.mj.stage').animate({ 'top': '-700px' }, 500);
+    $('.mj.michael-jackson').animate({ 'right': '800px' }, 1000); 
+    $('.mj.mic-stand').animate({ 'bottom': '-210px' }, 500);
+    $('.mj.disco-ball').animate({ 'top': '-270px' }, 500);
+
+    $('.ma.boxing-ring').animate({'bottom':'0px'}, 500);
+    $('.ma.muhammad-ali').animate({ 'right': '150px' }, 2000);
+    $('.ma.speed-bag').animate({ 'top': '100px' }, 1000);
+    $('.ma.fight-bell').animate({ 'top': '0px' }, 2000);
+}
+
+// slide4();
 
 /*-------------------------------------
 | Arrow Buttons
@@ -73,18 +106,16 @@ $('.arrow.right').click(right_arrow_click);
 
 // subtract 1
 function left_arrow_click() {
+
     currentSlide = currentSlide - 1;
     if (currentSlide == 0) {
         currentSlide = 1;
     }
     console.log(currentSlide);
 
-    set_the_stage();
-
     move_to_slide();
 }
 $('.arrow.left').click(left_arrow_click);
-
 
 /*-------------------------------------
 | Move to Chosen Slide
@@ -93,10 +124,13 @@ function move_to_slide() {
     if (currentSlide == 1) {
         set_the_stage();
     } else if (currentSlide == 2) {
+        
         slide2();
     } else if (currentSlide == 3) {
+        
         slide3();
     } else if (currentSlide == 4) {
+
         slide4();
     }
 }
